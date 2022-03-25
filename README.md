5e48639c268f3f785acbffe8c2fb65e4fc015637
Jon Tmarz <34428004+Jonedi@users.noreply.github.com>
2022-03-25 12:28:40 -0500

    creación proyecto

---
+++ README.md
@@ -0,0 +1,33 @@
+# Cliente/Servidor
+El siguiente proyecto se desarrollo para cumplir con la actividad 3 de la Unidad 1 de la materia Arquitectura de Software.
+
+El proyecto trata de un formulario con las siguientes indicaciones:
+
+- caja de texto: Agregar una tarea
+- Aplicación: elegir "JavaScript", "NodeJS" o elegir una de las dos
+- Prioridad: Elegir una prioridad "urgente" o "Relax"
+- Hacer clic en procesar para enviar los datos.
+
+los datos se guardarán en el localStorage del navegador, así que toda la vista, creación, edición y eliminación de datos se hará dentro del navegador.
+
+## Instalar paquetes
+Primero hay que instalar los paquetes con los que el servidor ejecutará las rutinas. Para realizar esto, en una ventana de comandos ("Command", "MS-DOS", "Console" "GitBash", etc), dirigirse a la carpeta donde se encuentra el proyecto y digitar el siguiente comando:
+```
+npm install
+```
+
+### Compilar e iniciar servidor para desarollo
+Una vez terminada la instalación de paquetes, el siguiente paso es iniciar el servidor escribiendo el comando en la ventana de comandos:
+```
+npm run serve
+```
+Esto creará un socket para acceder desde el navegador. Al finalizar dará la ruta de acceso al servidor, se puede copiar y pegar en un navegador o (ctrl + click izquierdo) con el teclado y ratón. Esto iniciará una ventana en el navegador y mostrará el proyecto
+
+### Compilar y minificar para producción
+Si se desea subir este proyecto en un servidor web y ejecutarlo en producción, se ejecuta el siguiente comando en la ventana de comandos:
+```
+npm run build
+```
+
+### Desarrollado por Jon Tmarz
+Ver [más proyectos](https://github.com/Jonedi).
---
+++ src/components/Input.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 mx-auto">
+        <input 
+          type="text"
+          class="form-control my-2"
+          placeholder="Ingrese una tarea"
+          v-model.trim="tarea.nombre"
+        >
+
+        <div class="form-check form-check-inline">
+          <input 
+            type="checkbox"
+            id="check-1"
+            class="form-check-input"
+            v-model="tarea.categorias"
+            value="javascript"
+          >
+          <label for="check-1" class="form-check-label">Javascript</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input 
+            type="checkbox"
+            id="check-2"
+            class="form-check-input"
+            v-model="tarea.categorias"
+            value="node js"
+          >
+          <label for="check-2" class="form-check-label">Node.js</label>
+        </div>
+
+        <div class="mt-2">
+          <div class="form-check form-check-inline">
+            <input 
+              type="radio"
+              id="radio-1"
+              class="form-check-input"
+              value="urgente"
+              v-model="tarea.estado"
+            >
+            <label for="radio-1" class="form-check-label">Urgente</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input 
+              type="radio"
+              id="radio-2"
+              class="form-check-input"
+              value="relax"
+              v-model="tarea.estado"
+            >
+            <label for="radio-2" class="form-check-label">Relax</label>
+          </div>
+        </div>
+
+        <div class="mt-2">
+          <input 
+            type="number"
+            class="form-control"
+            v-model.number="tarea.numero"
+          >
+        </div>
+
+        <button 
+          class="btn btn-dark mt-2" 
+          type="submit"
+          :disabled="bloquear"
+        >
+          Procesar
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tarea: Object
+    },
+    computed: {
+        bloquear(){
+            return this.tarea.nombre.trim() === "" ? true : false
+        }
+  }
+}
+</script>
